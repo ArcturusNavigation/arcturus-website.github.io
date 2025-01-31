5ad81706267ada91106663b5a5feca270eb19b67
Amy S <amys0930@gmail.com>
2025-01-31 15:36:54 -0500

    removed jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,10 +210,3 @@ document.addEventListener("scroll", function () {
     }
   }
 });
-
-jQuery(document).ready(function ($) {
-  $("li.dropdown a").click(function (e) {
-    $(this).next("ul.dropdown-menu").css("display", "block");
-    e.stopPropagation();
-  });
-});
